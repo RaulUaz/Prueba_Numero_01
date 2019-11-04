d964157fd2c70927d764158f46f1e3752654202d
LABDESSOFT <LABDESSOFT@LABDESSOFT-PC01>
2019-11-04 14:03:20 -0600

    Actualizacion_Numero_02

---
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author LABDESSOFT
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola esta es una actualizacion :)");
+    }
+    
+}
